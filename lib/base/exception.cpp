e565e8d6a0ffa9c30dd4c342df66cfa7ac6fface
Johannes Schmidt <johannes.schmidt@icinga.com>
2026-06-18 09:41:36 +0200

    Fix unreachable fallback in `DiagnosticInformation(<eptr>)`
    This was likely meant as a fallback for non-`std::exception`
    exception pointers, but it would never have been reached, because
    `(std|boost)::rethrow_exception()` would have thrown those out of
    the function scope.
    
    This fixes the fallback by making it a catch handler and also using
    the more direct way of getting this fallback diagnostic info inside
    a catch handler.

--- lib/base/exception.cpp
+++ lib/base/exception.cpp
@@ -298,9 +298,9 @@ String icinga::DiagnosticInformation(const std::exception_ptr& eptr, bool verbos
 		std::rethrow_exception(eptr);
 	} catch (const std::exception& ex) {
 		return DiagnosticInformation(ex, verbose, pt ? &stack : nullptr, pc ? &context : nullptr);
+	} catch (...) {
+		return boost::current_exception_diagnostic_information();
 	}
-
-	return boost::diagnostic_information(eptr);
 }
 
 ScriptError::ScriptError(String message)
